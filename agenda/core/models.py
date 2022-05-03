c0bae204ec1a28b526e33348125ffb5b054539a6
Rodrigo Érico <rodrigoerico25@gmail.com>
2022-05-03 09:27:24 -0400

    Adicionado usuario ao evento

--- agenda/core/models.py
+++ agenda/core/models.py
@@ -1,10 +1,13 @@
+from ast import mod
 from django.db import models
+from django.contrib.auth.models import User
 
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do evento')
     data_criacao = models.DateTimeField(auto_now=True, verbose_name='Data da criação')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     
     class Meta:
         db_table = 'evento'
