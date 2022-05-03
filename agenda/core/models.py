67740196a04d7c2295c63cded78ec6549d2d0638
Rodrigo Érico <rodrigoerico25@gmail.com>
2022-05-03 09:17:22 -0400

    Adicionado verbose_name para modificar osnomes dos titulos.

--- agenda/core/models.py
+++ agenda/core/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
-    data_evento = models.DateTimeField()
-    data_criacao = models.DateTimeField(auto_now=True)
+    data_evento = models.DateTimeField(verbose_name='Data do evento')
+    data_criacao = models.DateTimeField(auto_now=True, verbose_name='Data da criação')
     
     class Meta:
         db_table = 'evento'
