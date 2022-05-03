8a7013685bb661e77f637efafd622857b93f41ca
Rodrigo Érico <rodrigoerico25@gmail.com>
2022-05-03 09:13:40 -0400

    Criado funcao para exibir o titulo, data evento e data criacao.

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from core.models import Evento
 
-admin.site.register(Evento)
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+    
+    
+admin.site.register(Evento, EventoAdmin)
